5473bd5b1672538b798672e412e3f642ad6ffde5
aniket <aniketchavan505@gmail.com>
2026-03-05 16:19:28 +0530

    made connection with postgreSQL

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello from Node.js! 🚀");
+const express = require("express");
+const pool = require("./db");
+require("dotenv").config();
+
+const app = express();
+app.get("/users", async (req, res) => {
+  const result = await pool.query("SELECT * FROM users");
+  res.json(result.rows);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port 3000");
+});
